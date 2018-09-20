eb4c06316dad4e438f322be11db6f8c411bb7d95
wangxuquan <wangxuquan@meituan.com>
2018-09-20 12:09:18 +0800

    1

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 BUILD="build"
+NODE_MODULES="node_modules"
 if [ -d $BUILD ]; then
   rm -rf $BUILD
 fi
+if [ -d $NODE_MODULES ]; then
+  rm -rf $NODE_MODULES
+fi
+echo "npm installing..."
+npm i
 echo "webpack building..."
 npm run build
